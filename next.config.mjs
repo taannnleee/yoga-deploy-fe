db14271037731f5ec0520af1834ed433feaaa2e6
Tân <leetaan1902@gmail.com>
2024-11-25 14:26:11 +0700

    f

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,13 @@ const nextConfig = {
         ],
         dangerouslyAllowSVG: true,
     },
+
+    output: 'export', // Chuyển sang static export
+    assetPrefix: './', // Sử dụng đường dẫn tương đối
+
+    trailingSlash: true, // Thêm dấu "/" vào cuối các URL
+
 };
 
 export default withNextVideo(nextConfig);
+
